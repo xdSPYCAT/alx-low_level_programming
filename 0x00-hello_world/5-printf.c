56d92041287a39cc49293c66b8c14d388f266d85
mahmoud <mahmoudalnagar339@gmail.com>
2023-09-07 21:54:01 +0300

    haah

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *
- *Description: 'the program's description'
- *@parameter: describe the parameter
- *
  *Return: Always 0 (Success)
  */
 int main(void)
