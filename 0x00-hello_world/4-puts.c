455eef37535b10e5cea9044cc566b17e65dfcd13
mahmoud <mahmoudalnagar339@gmail.com>
2023-09-07 20:43:30 +0300

    haah

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main enty point
+ * return : always 0 (success)
+ */
+int main(void)
+{
+
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+}
