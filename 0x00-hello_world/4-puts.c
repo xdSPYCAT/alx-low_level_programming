0f86f36ab458ff78e43d56793fd2de79411f786a
mahmoud <mahmoudalnagar339@gmail.com>
2023-09-07 21:37:43 +0300

    haah

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
- *return: always 0 (success)
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
