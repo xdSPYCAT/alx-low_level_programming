0e6d6815b25ceb6996c00b181b4c341976142a74
mahmoud <mahmoudalnagar339@gmail.com>
2023-09-08 18:29:38 +0300

    haah

---
+++ 0x00-hello_world/testing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+ 
+int age;
+printf("enter age : ");
+scanf("%d",&age);
+if (age < 60)
+{
+printf("preeety young.");
+}
+else
+{
+printf("booommeerrrr");
+}
+return 0;
+
+}
