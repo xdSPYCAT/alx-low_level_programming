903ddac7bb336f21c777b03848f33ef404364913
mahmoud <mahmoudalnagar339@gmail.com>
2023-09-12 14:08:12 +0300

    haah

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int x = 97;
+
+	while (x > 123)
+	{
+		_putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
