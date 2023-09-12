df641da5fc667105a0bf3dac809835e0dec09795
mahmoud <mahmoudalnagar339@gmail.com>
2023-09-12 16:46:34 +0300

    haah

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,10 +28,7 @@ void times_table(void)
 
 			}
 
-
-
 			if (k < 10 && j != 0)
-
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -40,17 +37,12 @@ void times_table(void)
 			}
 			else if (k >= 10)
 			{
-
 				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-
 		}
-
 		_putchar('\n');
-
 	}
-
 }
