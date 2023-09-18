bb1e117ec99bb753b7cbb96602280be31fdf4247
mahmoud <mahmoudalnagar339@gmail.com>
2023-09-18 15:59:21 +0300

    haah

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns functions length
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+
+}
