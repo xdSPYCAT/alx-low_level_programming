b3c40d42d6fec5f4d3a5d01b0c3dfa1f09e675d4
mahmoud <mahmoudalnagar339@gmail.com>
2023-10-03 17:06:17 +0300

    haah

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *  * main - prints name
+ *   * @argc: number of arg
+ *    * @argv: array of arg
+ *     * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+
+	(void)argv;/*ignore argv*/
+	printf("%d\n" , argc - 1);
+
+	return (0);
+}
