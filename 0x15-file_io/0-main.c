184129a41f17c8a0b57f3661a90d037bd8042878
xdSPYCAT <mahmoudalnagar339@gmail.com>
2023-10-31 16:15:21 +0200

    haah

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -13,8 +13,8 @@ int main(int ac, char **av)
 
     if (ac != 2)
     {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
+	dprintf(2, "Usage: %s filename\n", av[0]);
+	exit(1);
     }
     n = read_textfile(av[1], 114);
     printf("\n(printed chars: %li)\n", n);
